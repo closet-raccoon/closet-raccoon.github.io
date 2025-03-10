7314fb64e61f7e5e8c870a393fb1a104736690dc
closet-raccoon <p.keagan@outlook.com>
2025-03-09 18:33:24 -0600

    Made the menu bar more consistant

--- resources/menubar.js
+++ resources/menubar.js
@@ -1,1 +1,2 @@
-const bar = '        <div style="margin: 0px; padding: 0px;" id = "menubar"><div class="menuboxcont">    <a class = "menubartitle" >Closet Server</a>    <div id="dropdown"><img id="chevron" src="/resources/test.svg"></img></div>    <div class = "menubox" ><a href="/" class="menuitemtext">Home</a></div>    <div class = "menubox" ><a href="/images/" class="menuitemtext">Featured</a></div>    <div class = "menubox" ><a href="https://cdn.closetraccoon.com"      target="_parent"  class="menuitemtext">All</a></div>    <div class = "menubox" ><a href="/social/" class="menuitemtext">Social</a></div>    <div class = "menubox" style="display: none;"><a href="/coding/" class="menuitemtext">Coding</a></div></div></div>'
+var clicked = false
+$("#menubar").load("/resources/bar.html")
